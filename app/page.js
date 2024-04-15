a1c50e501b7f996e4f6ff16b8b3d54419f34e685
Tanishq TaNiShQ-000 <tanishq.jaiswal000@gmail.com>
2024-04-15 20:50:04 +0530

    from branch

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1> hi there</h1>
-    
+      <h2> inside branch 2</h2>
     </main>
   );
 }
